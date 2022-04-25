57bf1475c19f79c769c9ae5598dae32df8b9efce
unknown <asmar.hajizada.std@bhos.edu.az>
2022-04-25 21:26:03 +0400

    uf

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var socketIO = require("socket.io")(http);
 var socketID = "";
 var users = [];
 
-var mainURL = "http://localhost:3000";
+var mainURL = "https://localhost:3000";
 
 socketIO.on("connection", function (socket) {
 	console.log("User connected", socket.id);
